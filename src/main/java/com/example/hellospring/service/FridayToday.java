ccbfbd55247d9460d89c51ebc7c09c617b09d664
Mohamad Ibrahim <mohamedibr@gmail.com>
2021-09-27 18:10:52 +0200

    refatored some code

--- src/main/java/com/example/hellospring/service/FridayToday.java
+++ src/main/java/com/example/hellospring/service/FridayToday.java
@@ -5,8 +5,6 @@ import java.time.LocalDate;
 public class FridayToday {
 
   private LocalDate date = LocalDate.now();
-
-  private int h;
   private int q = date.getDayOfMonth();
   private int m = date.getMonthValue();
   private int j = date.getYear()/100;
@@ -15,7 +13,7 @@ public class FridayToday {
   public int getDay(){
     int day;
 
-    this.h = (q +(13*(m+1)/5) + k + (k/4) + (j/4) -2*j)%7;
+    int h = (q +(13*(m+1)/5) + k + (k/4) + (j/4) -2*j)%7;
     day = ((h + 5)%7) + 1;
 
     return day;
